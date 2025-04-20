0b4764076253c9071c44a895bafc18cb6a374ec3
ArkynaSource <jhoelhex6@gmail.com>
2025-04-20 23:10:39 +0700

    another and another

--- Jhoel_122140174_pertemuan3/src/Jhoel_122140174_pertemuan3_reactSource/vite.config.js
+++ Jhoel_122140174_pertemuan3/src/Jhoel_122140174_pertemuan3_reactSource/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pemrograman_web_itera_122140174/Jhoel_122140174_pertemuan3/',
+  base: './',
   build: {
     outDir: 'build_output',
     emptyOutDir: true,
